29d6989f56e4c9bb820a6619d1636fe0bd549cdc
dan <xwj.harry@gmail.com>
2024-07-15 22:38:02 +0100

    update the test

--- tests/test_blasm_v2_v3_wGrid_transferFunction_generator.py
+++ tests/test_blasm_v2_v3_wGrid_transferFunction_generator.py
@@ -7,7 +7,8 @@ from learnedMethodForHologram.bandlimited_angular_spectrum_approach import (
     bandLimitedAngularSpectrumMethod_for_single_fixed_distance as BLASM_v3,
 )
 
-def test():
+
+def test(distance=torch.tensor([12e-3])):
     propagator_v2 = BLASM_v2(
         sample_row_num=192,
         sample_col_num=192,
@@ -18,7 +19,7 @@ def test():
     )
 
     w_grid_v2 = propagator_v2.generate_w_grid()
-    transfer_function_v2 = propagator_v2.generate_transfer_function(distances=torch.tensor([2.5e-3]))
+    transfer_function_v2 = propagator_v2.generate_transfer_function(distances=distance)
     # print(transfer_function_v2.shape)
 
     propagator_v3 = BLASM_v3(
@@ -28,7 +29,7 @@ def test():
         wave_length=torch.tensor([639e-9, 515e-9, 473e-9]),
         band_limit=False,
         cuda=False,
-        distance=torch.tensor([2.5e-3]),
+        distance=distance,
     )
 
     w_grid_v3 = propagator_v3.generate_w_grid()
@@ -37,6 +38,7 @@ def test():
 
     assert torch.allclose(w_grid_v2, w_grid_v3)
     assert torch.allclose(transfer_function_v2, transfer_function_v3)
+
 
 if __name__ == "__main__":
     sys.exit(test())
