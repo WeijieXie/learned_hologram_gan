815318a116dd33a4a077127f056f41c64fa7478b
dan <xwj.harry@gmail.com>
2024-07-25 00:14:27 +0100

    move the original watermelon to watermelon_v1

---
+++ learnedMethodForHologram/watermelon_v1.py
@@ -0,0 +1,126 @@
+import torch
+from torch import nn
+
+from .utilities import try_gpu
+from .bandlimited_angular_spectrum_approach import (
+    bandLimitedAngularSpectrumMethod_for_single_fixed_distance as BLASM_v3,
+)
+
+# BLASM_v1 denotes the class in optics.py,
+# BLASM_v2 denotes the parent class bandLimitedAngularSpectrumMethod in bandlimited_angular_spectrum_approach.py,
+# BLASM_v3 denotes the child class bandLimitedAngularSpectrumMethod_for_single_fixed_distance in bandlimited_angular_spectrum_approach.py
+
+from .neural_network_components import (
+    UNet_imgDepth2AP,
+    UNet_imgDepth2AP_heavyweight,
+    ResNet_POH,
+)
+
+
+class watermelon_v1(nn.Module):
+    def __init__(
+        self,
+        input_shape,
+        propagation_distance,
+        heavyweight_UNet=True,
+        cuda=True,
+    ):
+        super(watermelon_v1, self).__init__()
+
+        self.input_shape = input_shape
+        self.propagation_distance = propagation_distance
+        self.device = try_gpu() if cuda else torch.device("cpu")
+
+        # propagator
+        self.propagator = BLASM_v3(
+            sample_row_num=192,
+            sample_col_num=192,
+            pixel_pitch=3.74e-6,
+            wave_length=torch.tensor([639e-9, 515e-9, 473e-9]),
+            band_limit=False,
+            cuda=cuda,
+            distance=self.propagation_distance,
+        )
+
+        # a UNet used for generate amp and phs from rgbd input
+        if heavyweight_UNet:
+            self.part1 = UNet_imgDepth2AP_heavyweight(output_channels=6).to(self.device)
+        else:
+            self.part1 = UNet_imgDepth2AP(output_channels=6).to(self.device)
+
+        # a ResNet (without pooling) used for generate phase-only hologram from amp and phs
+        self.part2 = ResNet_POH(output_channels=3).to(self.device)
+
+        # Initialize weights
+        self._initialize_weights()
+
+    def forward(self, X):
+        # print(f"X shape is {X.shape}")
+        amp_phs_z = self.part1(X)
+        # print(f"amp_phs_z shape is {amp_phs_z.shape}")
+        amp_phs_0 = self.propagator.propagate_AP2AP(amp_phs_z)
+        # print(f"amp_phs_0 shape is {amp_phs_0.shape}")
+        phs_0 = self.part2(amp_phs_0)
+        # print(f"phs_0 shape is {phs_0.shape}")
+        intensity = self.propagator.propagate_P2I(phs_0)
+        # print(f"intensity shape is {intensity.shape}")
+        return intensity
+
+    def train_model(self, train_iter, test_iter, num_epochs, lr):
+        model = self
+        model.train()
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        for epoch in range(num_epochs):
+            model.train()
+            train_loss, n_train = 0.0, 0
+            for img_depth in train_iter:
+                y_hat = model(img_depth)
+                l = self.loss(
+                    y_hat, img_depth[:, :3]
+                )  # compare with the ground truth rgb image
+                self.optimizer.zero_grad()
+                l.backward()
+                self.optimizer.step()
+
+                train_loss += l.item()
+                n_train += img_depth.size(0)
+
+            self.eval()
+            test_loss, n_test = 0.0, 0
+            for img_depth in test_iter:
+                with torch.no_grad():
+                    y_hat = model(img_depth)
+                l = self.loss(y_hat, img_depth[:, :3])
+
+                test_loss += l.item()
+                n_test += img_depth.size(0)
+            print(
+                f"epoch {epoch + 1}, train loss {train_loss / n_train:.4f}, test loss {test_loss / n_test:.4f}"
+            )
+
+    def loss(self, y_hat, y):
+        loss = nn.MSELoss()
+        return loss(y_hat, y)
+
+    def _initialize_weights(self):
+        # Initialize weights by running a dummy forward pass
+        dummy_input = torch.randn(*self.input_shape).to(self.device)
+        _ = self.forward(dummy_input)
+
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.LazyConv2d)):
+                # nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, (nn.ConvTranspose2d, nn.LazyConvTranspose2d)):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, (nn.BatchNorm2d, nn.LazyBatchNorm2d)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, (nn.Linear, nn.LazyLinear)):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
